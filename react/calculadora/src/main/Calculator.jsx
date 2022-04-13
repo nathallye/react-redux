60d9e00f762510abb0ae496a260a5d8090ec5f19
Nathallye <nathallyet@gmail.com>
2022-04-13 00:14:20 -0300

    Projeto calc

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -1,11 +1,32 @@
-import './Calculator.css'
 import React, { Component } from "react";
+
+import "./Calculator.css";
+import Button from "../componets/Button";
+import Display from "../componets/Display";
 
 export default class Calculator extends Component {
   render() {
     return (
-      <div className='calculator'>
-        
+      <div className="calculator">
+        <Display value={100}/>
+
+        <Button label="AC"/>
+        <Button label="/"/>
+        <Button label="7"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="*"/>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="-"/>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+"/>
+        <Button label="0"/>
+        <Button label="."/>
+        <Button label="="/>
       </div>
     )
   }
